5f51bfc473dadc2e82f9284531768e68f31ac412
matttulio <101868697+matttulio@users.noreply.github.com>
2024-06-14 15:43:50 +0200

    Minor changes

--- plot_results.py
+++ plot_results.py
@@ -100,7 +100,7 @@ results.val_acc = results.val_acc.apply(ast.literal_eval)
 for model_type, g in results.groupby('model_type'):
     acc = []
     for i,row in g.iterrows():
-        plt.plot(row['val_acc'],c='orange')
+        plt.plot(row['val_acc'], c='orange')
         acc.append(row['val_acc'][-1])
     print(model_type, np.mean(acc), np.std(acc))
     plt.plot([],[],c='orange',label='validation')
