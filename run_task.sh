8b879afa1b54b718ef3e64b8df62e1c4bc7a5ac1
matttulio <101868697+matttulio@users.noreply.github.com>
2024-07-01 17:24:50 +0200

    Minor updates

--- run_task.sh
+++ run_task.sh
@@ -17,12 +17,13 @@ if [ "$task" -eq 1 ]; then
   echo "Running primitive NLP NTP task"
   num_samples=50000
   sequence_length=10
-  context_window=10
+  context_window=3
   vocab_size=78
   embedding_dim=50
   embedding_model='glove.6B.50d'
   seed=42
   distr_param=1.1
+  temperature=2
 
   python -u run_task.py \
     --task $task \
@@ -33,7 +34,8 @@ if [ "$task" -eq 1 ]; then
     --embedding_dim $embedding_dim \
     --embedding_model $embedding_model \
     --seed $seed \
-    --distr_param $distr_param
+    --distr_param $distr_param \
+    --temperature $temperature
 
 elif [ "$task" -eq 2 ]; then
   echo "Running primitive NLP summing task"
@@ -45,7 +47,8 @@ elif [ "$task" -eq 2 ]; then
   embedding_dim=50
   embedding_model='glove.6B.50d'
   seed=42
-  distr_param=2
+  distr_param=1.1
+  temperature=2
 
   python -u run_task.py \
     --task $task \
@@ -56,7 +59,8 @@ elif [ "$task" -eq 2 ]; then
     --embedding_dim $embedding_dim \
     --embedding_model $embedding_model \
     --seed $seed \
-    --distr_param $distr_param
+    --distr_param $distr_param \
+    --temperature $temperature
 
 elif [ "$task" -eq 3 ]; then
   echo "Running Next Histogram task"
